323d5c11f9293989192859319d20bd5cb11e0ebf
lam-phanminh <85839102+lam-phanminh@users.noreply.github.com>
2025-07-03 14:32:23 +0700

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "< 6.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "< 6.0.0"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+  }
+}
 
