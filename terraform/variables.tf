43652970ccc5dd412d9e30791e6a13395d89b1e8
lam-phanminh <85839102+lam-phanminh@users.noreply.github.com>
2025-07-03 15:42:01 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "project_id" {
+  type        = string
+  description = "The project ID to host the cluster in (required)"
+  default     = "infra-test"
+}
 
+variable "region" {
+  type        = string
+  description = "The region to host the cluster in (optional if zonal cluster / required if regional)"
+  default     = "asia-southeast1"
+}
+
+variable "env" {
+  type        = string
+  description = "The project name"
+  default     = "test"
+}
